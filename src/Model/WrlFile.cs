2a6b57c99618b17a4316d161a39447d1428b6f46
btigi <btigi@users.noreply.github.com>
2025-10-14 16:48:28 +0100

    Add WRL write method

---
+++ src/Model/WrlFile.cs
@@ -0,0 +1,18 @@
+﻿namespace ii.Min;
+
+public class WrlFile
+{
+    public string Signature { get; set; }
+    public Int16 Version { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public byte Unknown1 { get; set; }
+    public byte[] Unknown2 { get; set; }
+    public byte Unknown3 { get; set; }
+    public byte[] Unknown4 { get; set; }
+    public byte Unknown5 { get; set; }
+    public int TileCount { get; set; }
+    public byte[] TileData { get; set; }
+    public byte[] Palette { get; set; }
+    public byte[] TerrainInfo { get; set; } //  0 - land, 1 - water, 2 - shoreline, 3 - mountain
+}
--- src/WrlProcessor.cs
+++ src/WrlProcessor.cs
@@ -1,4 +1,4 @@
-﻿namespace iiMin;
+﻿namespace ii.Min;
 
 public class WrlProcessor
 {
@@ -8,67 +8,76 @@ public class WrlProcessor
         const int PaletteLength = 768;
 
         var result = new WrlFile();
+        using var fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+        using var br = new BinaryReader(fs);
 
-        try
+        result.Signature = new string(br.ReadChars(3));
+        if (result.Signature != "WRL")
         {
-            using var fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
-            using var br = new BinaryReader(fs);
-
-            result.Signature = new string(br.ReadChars(3));
-            if (result.Signature != "WRL")
-            {
-                //Console.WriteLine("File does not appear a WRL file");
-                return result;
-            }
-
-            result.Version = br.ReadInt16();
-            if (result.Version != 1)
-            {
-                //Console.WriteLine("File does not appear a supported WRL file version");
-                return result;
-            }
-
-            result.X = br.ReadInt16();
-            result.Y = br.ReadInt16();
-
-            result.Unknown1 = br.ReadByte(); // 1 byte null marker to separate contents
-            result.Unknown2 = br.ReadBytes(12544); // This seems a fixed size in all WRL files
-
-            result.Unknown3 = br.ReadByte(); // 1 byte null marker to separate contents
-            result.Unknown4 = br.ReadBytes(25085); // This seems a fixed size in all WRL files
-
-            result.Unknown5 = br.ReadByte(); // 1 byte null marker to separate contents
-            result.TileCount = br.ReadInt16();
-
-            var tileDataSize = result.TileCount * TileDimension * TileDimension;
-            result.TileData = br.ReadBytes(tileDataSize);
-
-            result.Palette = br.ReadBytes(PaletteLength);
-            //File.WriteAllBytes(Path.Combine(outputDirectory, "palette.pal"), paletteBytes);
-
-            result.TerrainInfo = br.ReadBytes(result.TileCount);
+            return result;
         }
-        catch
+
+        result.Version = br.ReadInt16();
+        if (result.Version != 1)
         {
-            Console.WriteLine("An unexpected error occured");
+            return result;
         }
+
+        result.X = br.ReadInt16();
+        result.Y = br.ReadInt16();
+
+        result.Unknown1 = br.ReadByte(); // 1 byte null marker to separate contents
+        result.Unknown2 = br.ReadBytes(12544); // This seems a fixed size in all WRL files
+
+        result.Unknown3 = br.ReadByte(); // 1 byte null marker to separate contents
+        result.Unknown4 = br.ReadBytes(25085); // This seems a fixed size in all WRL files
+
+        result.Unknown5 = br.ReadByte(); // 1 byte null marker to separate contents
+        result.TileCount = br.ReadInt16();
+
+        var tileDataSize = result.TileCount * TileDimension * TileDimension;
+        result.TileData = br.ReadBytes(tileDataSize);
+
+        result.Palette = br.ReadBytes(PaletteLength);
+
+        result.TerrainInfo = br.ReadBytes(result.TileCount);
         return result;
     }
 
-    public class WrlFile
+    public void Write(string filename, WrlFile wrlFile)
     {
-        public string Signature { get; set; }
-        public Int16 Version { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
-        public byte Unknown1 { get; set; }
-        public byte[] Unknown2 { get; set; }
-        public byte Unknown3 { get; set; }
-        public byte[] Unknown4 { get; set; }
-        public byte Unknown5 { get; set; }
-        public int TileCount { get; set; }
-        public byte[] TileData { get; set; }
-        public byte[] Palette { get; set; }
-        public byte[] TerrainInfo { get; set; } //  0 - land, 1 - water, 2 - shoreline, 3 - mountain
+        using var fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+        using var bw = new BinaryWriter(fs);
+
+        // Write signature (3 chars)
+        bw.Write(wrlFile.Signature.ToCharArray());
+
+        // Write version
+        bw.Write(wrlFile.Version);
+
+        // Write coordinates
+        bw.Write((short)wrlFile.X);
+        bw.Write((short)wrlFile.Y);
+
+        // Write first section
+        bw.Write(wrlFile.Unknown1);
+        bw.Write(wrlFile.Unknown2);
+
+        // Write second section
+        bw.Write(wrlFile.Unknown3);
+        bw.Write(wrlFile.Unknown4);
+
+        // Write tile information
+        bw.Write(wrlFile.Unknown5);
+        bw.Write((short)wrlFile.TileCount);
+
+        // Write tile data
+        bw.Write(wrlFile.TileData);
+
+        // Write palette
+        bw.Write(wrlFile.Palette);
+
+        // Write terrain info
+        bw.Write(wrlFile.TerrainInfo);
     }
 }
